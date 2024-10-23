c0cdb976b71697695ae9bcaa12df0cb6cd9e134c
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-10-22 23:25:50 -0400

    feat(student): call service student and show data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CardStudentComponent } from './components/card-student/card-student.component';
+import { StudentService } from './services/student.service';
+import { take, tap } from 'rxjs';
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
+import { IStudent } from './interfaces/IStudent';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    HttpClientModule,
+    CommonModule,
     CardModule,
     InputTextModule,
     ButtonModule,
@@ -19,6 +30,22 @@ import { CardStudentComponent } from './components/card-student/card-student.com
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fullstack-students-front';
+  students: IStudent[];
+
+  constructor(private readonly _studentService: StudentService) {}
+
+  ngOnInit(): void {
+    this._studentService
+      .getAllStudents()
+      .pipe(
+        take(1),
+        tap((students) => {
+          console.log(students);
+          this.students = students;
+        })
+      )
+      .subscribe();
+  }
 }
